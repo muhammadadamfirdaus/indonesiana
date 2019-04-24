4a726a8eda4229f339bf762b933b6cbc44e29b3d
muhammadadamfirdaus <adam@muhammadadamfirdaus>
2019-04-24 15:43:23 +0700

    mobile fix on cms

--- src/js/responsive.js
+++ src/js/responsive.js
@@ -1,9 +1,11 @@
-var isiPad = navigator.userAgent.match(/iPad/i) != null;
+// var isiPad = navigator.userAgent.match(/iPad/i) != null;
 
-
+var userAgent = navigator.userAgent.toLowerCase();
+const isTablet = /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(userAgent);
 var menuSidebar = document.querySelector('.dashboard');
-if(isiPad){
-  console.log(isiPad);
+var w = window.innerWidth;
+if(isTablet || w < 768){
+  console.log(isTablet);
   if(!menuSidebar.classList.contains('sidebar-hide')){
     menuSidebar.classList.add('sidebar-hide','ipad-tablet');
   }
